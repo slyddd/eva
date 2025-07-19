4677791a713db31b3e17f07847acc0157fb6f38f
slyddd <dayvidnavarrete@gmail.com>
2025-07-18 19:01:09 -0500

    Feat: Add modifications in fontsize based on font scale in the mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,55 @@
+const { fontScale } = require('nativewind/theme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
   theme: {
     extend: {
       borderRadius: {
-        "el-sm": "0.4rem",
-        "el-md": "0.8rem",
-        "el-lg": "2rem",
+        'el-sm': '0.4rem',
+        'el-md': '0.8rem',
+        'el-lg': '2rem',
       },
       colors: {
-        background: "rgb(var(--background) / <alpha-value>)",
-        foreground: "rgb(var(--foreground) / <alpha-value>)",
-        primary: "rgb(var(--primary) / <alpha-value>)",
-        surface: "rgb(var(--surface) / <alpha-value>)",
-        error: "rgb(var(--error) / <alpha-value>)",
-        success: "rgb(var(--success) / <alpha-value>)",
-        errorText: "rgb(var(--errorText) / <alpha-value>)",
-        successText: "rgb(var(--successText) / <alpha-value>)",
+        background: 'rgb(var(--background) / <alpha-value>)',
+        foreground: 'rgb(var(--foreground) / <alpha-value>)',
+        primary: 'rgb(var(--primary) / <alpha-value>)',
+        surface: 'rgb(var(--surface) / <alpha-value>)',
+        error: 'rgb(var(--error) / <alpha-value>)',
+        success: 'rgb(var(--success) / <alpha-value>)',
+        errorText: 'rgb(var(--errorText) / <alpha-value>)',
+        successText: 'rgb(var(--successText) / <alpha-value>)',
       },
+    },
+    fontSize: {
+      xs: fontScale(10),
+      sm: fontScale(12),
+      base: fontScale(14),
+      md: fontScale(14),
+      lg: fontScale(16),
+      xl: fontScale(18),
+      '2xl': fontScale(20),
+      '3xl': fontScale(24),
+      '4xl': fontScale(28),
+      '5xl': fontScale(32),
+      '6xl': fontScale(36),
+      '7xl': fontScale(40),
+      '8xl': fontScale(44),
     },
   },
   plugins: [
     ({ addBase }) =>
       addBase({
-        ":root": {
-          "--background": "255, 255, 255",
-          "--foreground": "0, 0, 0",
-          "--primary": "0, 122, 255",
-          "--surface": "240, 240, 240",
-          "--error": "255, 59, 48",
-          "--success": "52, 199, 89",
-          "--errorText": "255, 69, 58",
-          "--successText": "76, 217, 100",
+        ':root': {
+          '--background': '255, 255, 255',
+          '--foreground': '0, 0, 0',
+          '--primary': '0, 122, 255',
+          '--surface': '240, 240, 240',
+          '--error': '255, 59, 48',
+          '--success': '52, 199, 89',
+          '--errorText': '255, 69, 58',
+          '--successText': '76, 217, 100',
         },
       }),
   ],
